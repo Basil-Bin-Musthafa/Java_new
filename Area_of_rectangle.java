9bff0028b51240cd2b58d2713b35e456950d22b9
Basil-Bin-Musthafa <malappurambasil@gmail.com>
2024-12-23 19:15:15 +0530

    Area of rectanlge

---
+++ Area_of_rectangle.java
@@ -0,0 +1,15 @@
+public class Area_of_rectangle
+{
+    public static void main(String args[])
+    {
+        int length = 5;
+        int width = 3;
+        int area;
+
+        area = length * width;
+
+        System.out.println("Lenght =" + length);
+        System.out.println("Width =" + width);
+        System.out.println("Area of rectangle =" + area);
+    }
+}
